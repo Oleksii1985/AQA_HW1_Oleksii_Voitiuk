5d8b06f3bad6ae2975e40daa9d9234a4cf7553b0
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-06-23 13:57:47 +0300

    HW2_4

---
+++ homework_2/hw_2_4.py
@@ -0,0 +1,8 @@
+"""
+У вас есть две группы людей. В одной группе состоят всеядные люди в другой лишь вегетарианцы.
+Всеядный является вегетарианцем но вегетарианец не является всеядным.
+Вывести список гостей которые могут есть овощи и зелень.
+"""
+normal_people = ["Anna Smith", "John Snow", "Bob Mann", "Lisa Well"]
+vegetarian_people = ["Phil Lane", "Lisa Bum", "Eric Dow", "Melanie C"]
+total_people = normal_people + vegetarian_people
