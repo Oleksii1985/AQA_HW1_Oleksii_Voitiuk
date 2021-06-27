0b670475e98ba5e83b302b6475b2f35e02c7d272
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-06-27 14:49:53 +0300

    HW3_3

---
+++ homework_3/hw_3_3.py
@@ -0,0 +1,13 @@
+"""
+У вас есть 2 списка имен friends = ["John", "Marta", "James"] и enamies = ["John", "Johnatan", "Artur"].
+Перебрать имена друзей и написать сообщение f"{friend} we are the best friends" если друг не оказался в списке
+имен врагов. И вывести сообщение f"{friend} we are not the friends anymore" если друг оказался в списке врагов.
+Если имя друга James не проверяем его ибо он лучшый друг.
+"""
+friends = ["John", "Marta", "James"]
+enamies = ["John", "Johnatan", "Artur"]
+for friend in friends:
+    if friend not in enamies and friend != "James":
+        print(f"{friend} we are the best friends")
+    elif friend in enamies:
+        print(f"{friend} we are not the friends anymore")
