22ace0239cac6e691f844d8872bef9fa82c61b3f
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-07-04 19:56:21 +0300

    HW4_1

---
+++ homework_4/hw_4_1.py
@@ -0,0 +1,6 @@
+"""
+1 есть строка с именами разделенная пробелами "john marta james Morgan Adam Maria huang"
+преобразовать строку таким образом что бы каждое имя однозначно начиналось с большой буквы.
+"""
+some_string = "john marta james Morgan Adam Maria huang"
+some_string.title()
