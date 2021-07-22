0907382a5d3047217a0faf79b2d2c224e3c170d9
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-07-22 15:47:03 +0300

    HW9_2

---
+++ homework_9/hw_9_2.py
@@ -0,0 +1,11 @@
+"""
+Реализовать функцию которая возвращает дату недельной давности от текущего момента времени.
+"""
+from datetime import datetime, timedelta
+
+
+def subtract_one_week():
+    date_now = datetime.now()
+    seven_days = timedelta(7)
+    one_week_ago = date_now - seven_days
+    return one_week_ago
