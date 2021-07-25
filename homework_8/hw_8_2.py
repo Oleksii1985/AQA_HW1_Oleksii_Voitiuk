f2fda16ce8fb13240220f864c739e51cb34292df
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-07-25 17:46:43 +0300

    HW8_2_new

--- homework_8/hw_8_2.py
+++ homework_8/hw_8_2.py
@@ -4,9 +4,22 @@
 """
 
 
-def square_is_even():
-    result = (item ** 2 for item in range(0, 1000000001) if item % 2 == 0)
-    return result
+def my_gen():
+    counter = 0
+    while counter < 1000000000:
+        if counter % 2 == 0:
+            yield counter
+        counter += 1
+
+
+generator = my_gen()
+print(next(generator))
+print(next(generator))
+print(next(generator))
+
+
+result = (item ** 2 for item in range(0, 1000000001) if item % 2 == 0)
+
 
 # Well nice but I suggest use generator function or comprehension but not
 # all in one
