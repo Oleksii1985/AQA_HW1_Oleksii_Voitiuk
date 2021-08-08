42966f11f67aa6509d6b2310a3d3e422dc0961b6
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-08-08 08:14:50 +0300

    HW13_1_modify

--- homework_13/car_prius.py
+++ homework_13/car_prius.py
@@ -7,8 +7,14 @@ class CarPrius:
         self.__car_body_type = "Hatchback"
         self.__fuel_type = "Petrol"
 
+    def __clean_method(self, item):
+        pass
+
     def __str__(self):
+        res = ""
+        for key, value in self.__dict__.items():
+            res += f"{key}: {value}\n\t"
         return (
-            f"{self.__class__.__name__}\n"
-            f"{self.__dict__}"
+            f"{self.__class__.__name__}:{{\n"
+            f"\n\t{res}\n}}"
         )
