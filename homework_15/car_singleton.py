13993376cfa9c97a7e2c18c90e6bba818e437b4a
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-08-11 17:55:28 +0300

    HW15_1

---
+++ homework_15/car_singleton.py
@@ -0,0 +1,9 @@
+from typing import Type
+
+
+def singleton(_class: Type):
+    def inner(*args: str):
+        if not hasattr(_class, "__instance"):
+            setattr(_class, "__instance", _class(*args))
+        return getattr(_class, "__instance")
+    return inner
