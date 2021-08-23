1d14de440325751ef72809d482958489a7aaffc5
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2021-08-23 13:27:31 +0300

    HW16_fix

--- homework_16/conftest.py
+++ homework_16/conftest.py
@@ -5,14 +5,4 @@ from homework_16.human import Human
 
 @pytest.fixture(scope="module")
 def human() -> Human:
-    yield Human("Alex", 101, "male")
-
-
-@pytest.fixture(scope="module")
-def dead_human() -> Human:
-    yield Human("Vasyl", 99, "male")
-
-
-@pytest.fixture(scope="module")
-def human_without_gender() -> Human:
-    yield Human("Some", 30, "")
+    yield Human("Alex", 36, "male")
--- homework_16/test_human.py
+++ homework_16/test_human.py
@@ -3,44 +3,33 @@ import pytest
 from homework_16.human import Human
 
 
-def test_human_is_grow_up(human):
+def test_age(human):
+    assert human.age == 36
+
+
+def test_gender(human):
+    assert human.gender == "male"
+
+
+def test_grow_up(human):
     human.grow()
 
-    assert human.age == 37
+    assert human._Human__age == 37
 
 
-def test_human_is_dead(dead_human):
-    dead_human.grow()
-
-    assert dead_human._Human__status == "dead"
-
-
-def test_human_is_alive(human):
-    # well this step is excessive since return only true or false
-    # but if I will change conditions of fixture it will fail with Exception error
-
+def test_is_alive(human):
     human._Human__is_alive()
 
     assert human._Human__status == "alive"
 
 
-def test_human_is_alive_exception(dead_human):
-    with pytest.raises(Exception):
-        dead_human._Human__is_alive()
+def test_change_gender(human):
+    human.change_gender("female")
+
+    assert human._Human__gender == "female"
 
 
-@pytest.mark.parametrize(
-    "gender,expected",
-    [("male", "male"), ("female", "female")],
-    ids=["change_to_male", "change_to_female"]
-)
-def test_change_gender(human_without_gender, gender, expected):
-    human_without_gender.change_gender(gender)
-    # will be better to use monkey patch here
-    assert human_without_gender.gender == expected
-
-
-def test_human_change_gender_exception(human):
+def test_change_gender_exception(human):
     with pytest.raises(Exception):
         human.change_gender("")
 
@@ -48,3 +37,17 @@ def test_human_change_gender_exception(human):
 def test_validate_gender_exception(human):
     with pytest.raises(Exception):
         human._Human__validate_gender("")
+
+
+def test_grow_dead(human, monkeypatch):
+    monkeypatch.setattr(
+        human, "_Human__age", 101
+    )
+    human.grow()
+
+    assert human._Human__status == "dead"
+
+
+def test_is_alive_dead(human):
+    with pytest.raises(Exception):
+        human._Human__is_alive()
